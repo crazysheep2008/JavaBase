9e32bce9f7d15d1d2b8e719d9faf7f23213eca56
yangfeng <yangfeng3@beingmate.com>
2018-07-20 14:36:42 +0800

    update

--- src/main/java/com/beingmate/learn/algorithm/leetcode/array/FourNumSum.java
+++ src/main/java/com/beingmate/learn/algorithm/leetcode/array/FourNumSum.java
@@ -2,15 +2,35 @@ package com.beingmate.learn.algorithm.leetcode.array;
 
 import com.alibaba.fastjson.JSON;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /****
  * https://leetcode-cn.com/problems/4sum/description/
  */
 public class FourNumSum {
+
+    class Pair {
+        int l, r;
+
+        public Pair(int v1, int v2) {
+            this.l = v1;
+            this.r = v2;
+        }
+
+        public int sum() {
+            return l + r;
+        }
+    }
+
+    public List<Integer> buildResults(Pair p1, Pair p2) {
+        List<Integer> res = new ArrayList<>();
+        res.add(p1.l);
+        res.add(p1.r);
+        res.add(p2.l);
+        res.add(p2.r);
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] inputs = new int[]{1, 0, -1, 0, -2, 2};
         FourNumSum fns = new FourNumSum();
@@ -20,66 +40,29 @@ public class FourNumSum {
 
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> datas = new ArrayList();
-        if (nums == null || nums.length == 0) {
+        if (nums == null || nums.length < 4) {
             return datas;
         }
-        List<Integer> inputs = new ArrayList();
-        for (int v : nums) {
-            inputs.add(v);
+        int numLimit = 4;
+        List<Pair> outPairs = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - numLimit + 1; i++) {
+            for (int j = i + 1; j < nums.length - numLimit + 2; j++) {
+                Pair p = new Pair(nums[i], nums[j]);
+                outPairs.add(p);
+                Map<Integer, Pair> restMap = new HashMap();
+                for (int h = j + 1; h < nums.length - 1; h++) {
+                    for (int k = h + 1; k < nums.length; k++) {
+                        Pair p2 = new Pair(nums[h], nums[k]);
+                        restMap.put(p2.sum(), p2);
+                    }
+                }
+                Pair restPair = restMap.get(target - p.sum());
+                if (restPair != null) {
+                    datas.add(buildResults(p, restPair));
+                }
+            }
         }
-        return findNums(inputs, target, 4);
+        return datas;
     }
-
-    private List<List<Integer>> findNums(List<Integer> nums, int target, int numCount) {
-        List<List<Integer>> results = new ArrayList();
-        if (nums.isEmpty() || nums.size() < numCount) {
-            return results;
-        }
-        if (numCount == 1){
-            List<Integer> subList = new ArrayList();
-            for (int i = 0; i < nums.size(); i++) {
-                int num = nums.get(i);
-               if (target == num){
-                   subList.add(num);
-                   results.add(subList);
-                   return results;
-               }
-            }
-            return results;
-        }
-        if (numCount == nums.size()) {
-            List<Integer> subList = new ArrayList();
-            int sum = 0;
-            for (int i = 0; i < nums.size(); i++) {
-                int num = nums.get(i);
-                sum = sum + num;
-                subList.add(num);
-            }
-            if (sum == target) {
-                results.add(subList);
-            }
-            return results;
-        }
-
-        Set<Integer> numSet = new HashSet();
-        for (int i = 0; i < nums.size() - numCount + 1; i++) {
-            int curNum = nums.get(i);
-            if (numSet.contains(curNum)) {
-                continue;
-            }
-            numSet.add(curNum);
-
-            int rest = target - curNum;
-            List<List<Integer>> subNums = findNums(nums.subList(i + 1, nums.size()), rest, numCount - 1);
-            if (subNums.isEmpty()) {
-                continue;
-            }
-            for (List<Integer> subList : subNums) {
-                subList.add(curNum);
-                results.add(subList);
-            }
-        }
-        return results;
-    }
-
 }
